ac1f27ae9d820fe81fe03454a403b4cf81e28748
daw <daw@daw-VirtualBox>
2017-11-03 13:09:28 +0100

    Merge origin/master
    Conflicts:
    	README.md

---
+++ JavaArrays/src/javaarrays/JavaArrays.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaarrays;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author daw
+ */
+public class JavaArrays {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    System.out.println("Cuántos números quieres introducir");
+    Scanner sc = new Scanner(System.in);
+    int numerodenumeros=sc.nextInt();
+    int [] numeros= new int [numerodenumeros];
+    int numerodepares=0; int numerodeimpares=0;
+    int contadorpares=0; int contadorimpares=0;
+    for(int i=0;i<numerodenumeros;i++){
+        numeros[i] =sc.nextInt();   
+    }
+    
+    for(int i=0;i<numerodenumeros;i++){
+        if ((numeros[i]%2)==0){
+            numerodepares++;
+        }
+        else{
+            numerodeimpares++;
+        }
+    }
+    int [] pares= new int [numerodepares];
+    int [] impares= new int [numerodeimpares];
+    
+    for(int i=0;i<numerodenumeros;i++){
+        if ((numeros[i]%2)==0){
+           pares[contadorpares]=numeros[i];
+           contadorpares++;
+        } 
+        else{
+           impares[contadorimpares]=numeros[i];
+        contadorimpares++;
+        } 
+    }
+    int acum=0;
+    for(int i=0;i<pares.length;i++){
+        acum+=pares[i];
+    }
+    System.out.println("La media de los pares es "+(acum/pares.length));
+    acum=0;
+    for (int i=0;i<impares.length;i++){
+    acum+=impares[i];
+    }
+    System.out.print("La media de los impares es "+(acum/impares.length));
+
+        
+        
+    
+    
+    
+    }
+    
+}
