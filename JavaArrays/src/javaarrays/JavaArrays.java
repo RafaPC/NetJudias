b3fa1cce4d7da4d0b422132797447450845cbdf8
daw <daw@daw-VirtualBox>
2017-11-06 10:33:41 +0100

    Merge origin/master
    Conflicts:
    	JavaApplication2/src/javaapplication2/Arrays.java

--- JavaArrays/src/javaarrays/JavaArrays.java
+++ JavaArrays/src/javaarrays/JavaArrays.java
@@ -17,9 +17,81 @@ public class JavaArrays {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Elige programa: ");
+        int programa;
+        programa = sc.nextInt();
+        switch (programa) {
+
+            case 1:
+                ejercicio1(sc);
+                break;
+            case 2:
+                ejercicio2(sc);
+                break;
+            /*case 3:
+                ejercicio3(sc);
+                break;
+            case 4:
+                ejercicio4(sc);
+                break;
+            case 5:
+                ejercicio5(sc);
+                break;
+            case 6:
+                ejercicio6(sc);
+                break;
+            case 7:
+                ejercicio7(sc);
+                break;
+            case 8:
+                ejercicio8(sc);
+                break;
+            case 9:
+                ejercicio9(sc);
+                break;
+            case 10:
+                ejercicio10(sc);
+                break;
+            case 11:
+                ejercicio11(sc);
+                break;
+            case 12:
+                ejercicio12(sc);
+                break;
+            case 13:
+                ejercicio13(sc);
+                break;*/
+            default: System.out.println("Has elegido un programa que no existe");
+        }
+    }
+    public static void ejercicio1(Scanner sc){
+        int numeros[]=new int [5];
+        for(int i=0;i<5;i++){
+            System.out.println("Dame un número: "); 
+            numeros[i]=sc.nextInt();
+        }
+        System.out.println("Ahí van tus números");
+        for(int i=0;i<5;i++){
+            System.out.println(numeros[i]);
+        }
+    }
+    public static void ejercicio2(Scanner sc){
+        int numeros[]=new int [5];
+        for(int i=0;i<5;i++){
+            System.out.println("Dame un número: "); 
+            numeros[i]=sc.nextInt();
+        }
+        System.out.println("Ahí van tus números al revés");
+        for(int i=4;i>-1;i--){
+            System.out.println(numeros[i]);
+        }
+    }
+    
+    
+    
+    /*public static void ejercicioX(Scanner sc){
     System.out.println("Cuántos números quieres introducir");
-    Scanner sc = new Scanner(System.in);
     int numerodenumeros=sc.nextInt();
     int [] numeros= new int [numerodenumeros];
     int numerodepares=0; int numerodeimpares=0;
@@ -59,13 +131,12 @@ public class JavaArrays {
     for (int i=0;i<impares.length;i++){
     acum+=impares[i];
     }
-    System.out.print("La media de los impares es "+(acum/impares.length));
+    System.out.print("La media de los impares es "+(acum/impares.length));*/
 
         
         
     
-    
-    
-    }
+       
     
 }
+    
