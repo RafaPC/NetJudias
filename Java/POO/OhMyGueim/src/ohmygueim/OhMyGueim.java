b400d4232a977082b7e2f32cff1075d49b547024
daw <daw@b25-25>
2018-02-26 10:27:56 +0100

    Merge origin/master
    Conflicts:
    	.gitignore
    	HTML5/Materialize&Bootstrap/public_html/bootstrap4.html
    	HTML5/Materialize&Bootstrap/public_html/idesweb.html
    	Java/POO/Biblioteca/nbproject/private/private.properties

--- Java/POO/OhMyGueim/src/ohmygueim/OhMyGueim.java
+++ Java/POO/OhMyGueim/src/ohmygueim/OhMyGueim.java
@@ -17,21 +17,21 @@ import ohmygueim.Clientes.LOL;
  * @author daw
  */
 public class OhMyGueim {
-    private ArrayList <Gamer> clientes;
+    private ArrayList <Gamer> gamers;
     public OhMyGueim() {
-       clientes.add(new CSGO("Cloud9",567,"Dust 2",2000,"Carceus",56));
-       clientes.get(0).addTrofeo("Pìston Cup");
+       gamers.add(new CSGO("Cloud9",567,"Dust 2",2000,"Carceus",56));
+       gamers.get(0).addTrofeo("Pìston Cup");
        
-       clientes.add(new LOL("Mid","Asesino",2300,"xPeke",72));
-       clientes.get(1).addTrofeo("DreamHack 2016");
-       clientes.add(new ClashRoyale(2000,"Ocelote",64));
-       ((ClashRoyale)clientes.get(2)).addCarta("Gigante", 1);
-       ((ClashRoyale)clientes.get(2)).addCarta("Mago", 2);
-       ((ClashRoyale)clientes.get(2)).addCarta("Mago eléctrico", 4);
-       ((ClashRoyale)clientes.get(2)).addCarta("Monta Puercos", 1);
-       ((ClashRoyale)clientes.get(2)).addCarta("P.E.K.K.A.", 3);
-       ((ClashRoyale)clientes.get(2)).addCarta("Bruja", 4);
-       ((ClashRoyale)clientes.get(2)).addCarta("Arqueras", 1);
+       gamers.add(new LOL("Mid","Asesino",2300,"xPeke",72));
+       gamers.get(1).addTrofeo("DreamHack 2016");
+       gamers.add(new ClashRoyale(2000,"Ocelote",64));
+       ((ClashRoyale)gamers.get(2)).addCarta("Gigante", 1);
+       ((ClashRoyale)gamers.get(2)).addCarta("Mago", 2);
+       ((ClashRoyale)gamers.get(2)).addCarta("Mago eléctrico", 4);
+       ((ClashRoyale)gamers.get(2)).addCarta("Monta Puercos", 1);
+       ((ClashRoyale)gamers.get(2)).addCarta("P.E.K.K.A.", 3);
+       ((ClashRoyale)gamers.get(2)).addCarta("Bruja", 4);
+       ((ClashRoyale)gamers.get(2)).addCarta("Arqueras", 1);
        //LocalDate temp = new LocalDate(13,11,2018);
     }
 
