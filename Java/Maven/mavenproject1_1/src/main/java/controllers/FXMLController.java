080bfe13dc15b12f018e8ff6f53ee27226df91a0
daw <daw@b25-25>
2018-03-16 13:30:30 +0100

    Merge origin/master
    Conflicts:
    	.gitignore
    	HTML5/Materialize&Bootstrap/public_html/bootstrap4.html
    	HTML5/Materialize&Bootstrap/public_html/idesweb.html
    	Java/POO/Biblioteca/nbproject/private/private.properties

--- Java/Maven/mavenproject1_1/src/main/java/controllers/FXMLController.java
+++ Java/Maven/mavenproject1_1/src/main/java/controllers/FXMLController.java
@@ -7,15 +7,12 @@ package controllers;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import modelo.Cliente;
 
 /**
  * FXML Controller class
@@ -27,62 +24,13 @@ public class FXMLController implements Initializable {
     /**
      * Initializes the controller class.
      */
-    List<Cliente> clientes;
-    private int indice;
-
-    @FXML
-    private Label fxUser;
-
-    @FXML
-    private Label fxEdad;
-
-    @FXML 
-    private Button fxSiguiente;
     
-    @FXML 
-    private Button fxAnterior;
-    
-    @FXML
-    private void mostrarCliente() throws IOException {
-        fxUser.setText(clientes.get(indice).getNombre());
-        fxEdad.setText(clientes.get(indice).getEdad() + "");
-    }
 
-    @FXML
-    private void derecha(ActionEvent event) throws IOException {
-        if (indice == clientes.size()) {
-            fxSiguiente.setDisable(true);
-        } else {
-            indice++;
-            if(fxAnterior.isDisable()){
-                fxAnterior.setDisable(false);
-            }
-        }
-        mostrarCliente();
-    }
 
-    @FXML
-    private void izquierda(ActionEvent event) throws IOException {
-        if (indice == 0) {
-            fxAnterior.setDisable(true);
-        } else {
-            indice--;
-            if(fxSiguiente.isDisable()){
-                fxSiguiente.setDisable(false);
-            }
-        }
-        mostrarCliente();
-    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        clientes = new LinkedList<>();
-        indice = 0;
-        clientes.add(new Cliente("Kiko", 18));
-        clientes.add(new Cliente("Andrea", 20));
-        clientes.add(new Cliente("Yáñez", 18));
-        fxUser.setText(clientes.get(indice).getNombre());
-        fxEdad.setText(clientes.get(indice).getEdad() + "");
+        
     }
 
 }
--- Java/Maven/mavenproject1_1/src/main/java/main/NewFXMain1.java
+++ Java/Maven/mavenproject1_1/src/main/java/main/NewFXMain1.java
@@ -13,6 +13,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -21,33 +23,24 @@ import javafx.stage.Stage;
  * @author daw
  */
 public class NewFXMain1 extends Application {
-    
+
     @Override
-    public void start(Stage primaryStage) throws IOException{
-        /*Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);*/
-        
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/FXML.fxml"));
-        
+    public void start(Stage primaryStage) throws IOException {
+        BorderPane root = FXMLLoader.load(getClass().getResource("/resources/fxml/FXMLMenu.fxml"));
+
+        AnchorPane anchor;
+        //load up OTHER FXML document
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/resources/fxml/FXMLScene.fxml"));
+        anchor = loader.load();
+        FXMLSceneController controller = loader.getController();
+        root.setCenter(anchor);
         Scene scene = new Scene(root);
-        scene.getStylesheets().add("/styles/fxml.css");
-        
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        scene.getStylesheets().add("/css/fxmlscene.css");
+
+        stage.setTitle("JavaFX and Maven");
+        stage.setScene(scene);
+        stage.show();
     }
 
     /**
@@ -56,5 +49,5 @@ public class NewFXMain1 extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
