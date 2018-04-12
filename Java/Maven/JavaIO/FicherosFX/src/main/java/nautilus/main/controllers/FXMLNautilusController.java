19283bc85a155c071826f92965699921eb513868
daw <daw@b25-25>
2018-04-12 13:00:20 +0200

    Merge origin/master
    Conflicts:
    	.gitignore
    	HTML5/Materialize&Bootstrap/public_html/bootstrap4.html
    	HTML5/Materialize&Bootstrap/public_html/idesweb.html
    	Java/POO/Biblioteca/nbproject/private/private.properties

---
+++ Java/Maven/JavaIO/FicherosFX/src/main/java/nautilus/main/controllers/FXMLNautilusController.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nautilus.main.controllers;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+/**
+ * FXML Controller class
+ *
+ * @author user
+ */
+public class FXMLNautilusController implements Initializable {
+
+    @FXML
+    private ListView<File> fxFiles;
+    @FXML
+    private Label fxRutaActual;
+
+    @FXML
+    private Button fxBotonSubir;
+
+    private String rutaActual;
+
+    private File seleccionado;
+
+    @FXML
+    public void handleSubir(ActionEvent event) {
+
+        if (fxRutaActual.getText().equalsIgnoreCase("/")) {
+            fxBotonSubir.setDisable(true);
+        } else {
+            fxRutaActual.setText(seleccionado.getParent());
+            
+        }
+
+    }
+
+    @FXML
+    public void handleEntrar(ActionEvent event) {
+
+        File seleccionado
+                = fxFiles.getSelectionModel().getSelectedItem();
+
+        fxRutaActual.setText(seleccionado.getAbsolutePath());
+        fxBotonSubir.setDisable(false);
+        cargarFiles();
+
+    }
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        rutaActual = "/";
+        fxRutaActual.setText(rutaActual);
+        cargarFiles();
+
+        // TODO
+    }
+
+    private void cargarFiles() {
+        File f = new File(fxRutaActual.getText());
+        fxFiles.getItems().clear();
+        fxFiles.getItems().addAll(f.listFiles());
+    }
+}
