d6ddc137119da05295b42677850b12156c257766
daw <daw@b25-25>
2018-04-25 14:28:47 +0200

    Merge origin/master
    Conflicts:
    	.gitignore
    	HTML5/Materialize&Bootstrap/public_html/bootstrap4.html
    	HTML5/Materialize&Bootstrap/public_html/idesweb.html
    	Java/POO/Biblioteca/nbproject/private/private.properties

---
+++ Java/BBDD/BaseDatos/src/main/java/dao/ConexionSimpleBD.java
@@ -0,0 +1,151 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import config.Configuration;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Alumno;
+import model.Asignatura;
+
+/**
+ *
+ * @author oscar
+ */
+public class ConexionSimpleBD {
+
+    // Select JDBC
+    public List<Alumno> getAllAlumnosJDBC() {
+        List<Alumno> lista = new ArrayList<>();
+        Alumno nuevo = null;
+
+        Connection con = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            Class.forName(Configuration.getInstance().getDriverDB());
+            
+
+            con = DriverManager.getConnection(
+                    Configuration.getInstance().getUrlDB(),
+                    Configuration.getInstance().getUserDB(),
+                    Configuration.getInstance().getPassDB());
+
+            stmt = con.createStatement();
+            String sql;
+            
+            sql = "SELECT * FROM alumnos";
+            rs = stmt.executeQuery(sql);
+
+            //STEP 5: Extract data from result set
+            while (rs.next()) {
+                //Retrieve by column name
+                int id = rs.getInt("id");
+                String nombre = rs.getString("nombre");
+                Date fn = rs.getDate("fecha_nacimiento");
+                Boolean mayor = rs.getBoolean("mayor_edad");
+                nuevo = new Alumno();
+                nuevo.setFecha_nacimiento(fn);
+                nuevo.setId(id);
+                nuevo.setMayor_edad(mayor);
+                nuevo.setNombre(nombre);
+                lista.add(nuevo);
+            }
+
+        } catch (Exception ex) {
+            Logger.getLogger(AlumnosDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(AlumnosDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+        return lista;
+
+    }
+    public List<Asignatura> getAllAsignaturasJDBC() {
+        List<Asignatura> lista = new ArrayList<>();
+        Asignatura nuevo = null;
+
+        Connection con = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            Class.forName(Configuration.getInstance().getDriverDB());
+            
+
+            con = DriverManager.getConnection(
+                    Configuration.getInstance().getUrlDB(),
+                    Configuration.getInstance().getUserDB(),
+                    Configuration.getInstance().getPassDB());
+
+            stmt = con.createStatement();
+            String sql;
+            
+            sql = "SELECT * FROM asignaturas";
+            rs = stmt.executeQuery(sql);
+
+            //STEP 5: Extract data from result set
+            while (rs.next()) {
+                //Retrieve by column name
+                int id = rs.getInt("id");
+                String nombre = rs.getString("nombre");
+                String curso = rs.getString("curso");
+                String ciclo = rs.getString("ciclo");
+                nuevo = new Asignatura();
+                nuevo.setId(id);
+                nuevo.setNombre(nombre);
+                nuevo.setCurso(curso);
+                nuevo.setCiclo(ciclo);
+                lista.add(nuevo);
+            }
+
+        } catch (Exception ex) {
+            Logger.getLogger(ConexionSimpleBD.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(AlumnosDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+        return lista;
+
+    }
+    /*public Alumno getAlumnoJDBC(String id)
+      
+    {
+           
+    }*/
+
+}
---
+++ Java/BBDD/BaseDatos/src/main/java/main/Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import dao.ConexionSimpleBD;
+import java.util.List;
+import model.Alumno;
+import model.Asignatura;
+
+/**
+ *
+ * @author user
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        ConexionSimpleBD c = new ConexionSimpleBD();
+
+        List<Asignatura> asignaturas = c.getAllAsignaturasJDBC();
+        for (Asignatura a : asignaturas) {
+            System.out.println("ID: " + a.getId());
+            System.out.println("Nombre: " + a.getNombre());
+            System.out.println("Curso: " + a.getCurso());
+            System.out.println("Ciclo: " + a.getCiclo());
+            System.out.println("-------------------------");
+        }
+    }
+}
